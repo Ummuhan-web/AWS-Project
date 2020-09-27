3ce0c50a1bf451dac6a8ca731d2e56b0a3c503ed
UMMUHAN <umm.yvz@gmail.com>
2020-09-27 23:43:52 +0000

    I add  python3 app.py

---
+++ covid.app.py
@@ -0,0 +1,9 @@
+from flask import Flask, render_template, request
+
+app = Falsk(__name__)
+
+def converter(user_age):
+    if user_age > 75 or cronic == 'yes' :
+     return "You are in risky group"
+    else :
+     return "You are not in risky group"
